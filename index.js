60c0b43782a1cb5fb764c8178b72b19ee07066ef
Frank <frank@smartman.com.tw>
2024-11-20 10:09:41 +0800

    Add 九暘 credential

--- index.js
+++ index.js
@@ -417,7 +417,7 @@ function handleFileUpload(event) {
   try {
     // prompt the user to enter a password
     const password = prompt("請輸入密碼");
-    if (password !== "70742842" && password !== "29039617") {
+    if (password !== "70742842" && password !== "29039617" && password !== "16069953") {
       alert("密碼錯誤");
       return;
     }
